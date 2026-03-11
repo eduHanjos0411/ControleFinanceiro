18f277f4bce01ce3f9903179bc86a46e5e012ef6
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-03-11 12:09:59 -0300

    feat: adding type check

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,4 +1,6 @@
 export function formatCurrency(value: number): string {
+  if (typeof value != "number") {return ""}
+
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
