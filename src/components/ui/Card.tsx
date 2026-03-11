c7cab8ad231087981b27f1a88a767f089b3bfb38
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-03-11 12:07:28 -0300

    feat: adding popup

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,8 +1,8 @@
 interface BalanceCardProps {
-  title: string
-  value: number
-  lastValue?: number
-  popup?: React.ReactNode
+  title: string;
+  value: number;
+  lastValue?: number;
+  popup?: React.ReactNode;
 }
 
 import type React from "react";
@@ -10,22 +10,25 @@ import { formatCurrency } from "../../utils/formatCurrency";
 import "./card.css";
 
 export function Card({ title, value, lastValue, popup }: BalanceCardProps) {
-  if (typeof lastValue != "number") return "Falha ao carregar valores"
-  
+  if (typeof lastValue != "number") return "erro ao carregar dados";
+
   const isPositive = lastValue >= 0;
-  console.log(isPositive)
+  console.log(isPositive);
 
   return (
     <div className="balance-card-container">
-      <h3 className="card-title">{title}</h3>
+      <div className="top-site">
+        <h3 className="card-title">{title}</h3>
+        <div className="icon">
+          ⓘ
+          <div className="popup">{popup}</div>
+        </div>
+      </div>
       <p className="card-value">{formatCurrency(value)}</p>
       <span className={`card-detail ${isPositive ? "positive" : "negative"}`}>
         {isPositive ? "+" : ""}
         {formatCurrency(lastValue)}
       </span>
-      <div>
-        {popup}
-      </div>
     </div>
   );
 }
