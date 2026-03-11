d297a369d9338ab552fffa8823f85292bf49f759
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-03-11 12:08:18 -0300

    feat: transactions page and components

--- src/features/transactions/TransactionsPage.tsx
+++ src/features/transactions/TransactionsPage.tsx
@@ -1,6 +1,13 @@
+import { TransactionTable } from "./components/TransactionTable";
+import "./transactionsPage.css";
+
 export function TransactionsPage() {
   return (
-    <>
-    </>
-  )
+    <div className="transactions-page">
+      <div className="transactions-actions">
+        <button className="trans-act-btn">Adicionar</button>
+      </div>
+      <TransactionTable/>
+    </div>
+  );
 }
